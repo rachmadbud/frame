5e754d8a7b6b6762fd9ead15727a062faecd5ed6
rachmadbud <rachmad.16421044@student.ubl.ac.id>
2024-09-28 19:41:37 +0700

    home url

--- app/config/config.php
+++ app/config/config.php
@@ -1,11 +1,9 @@
 <?php
 
-define('BASEURL', 'http://localhost/websiteaku');
+define('BASEURL', 'http://localhost/frame');
 
 // DB
 define('DB_HOST', 'mysql:host=localhost;dbname=blog');
 define('DB_USER', 'root');
 define('DB_PASS', '');
 define('DB_NAME', 'miniframework');
-
-
